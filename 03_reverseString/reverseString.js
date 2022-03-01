61d5ebdabbd53d8cd2eb79b60fa7553f821a2061
Dan <en07djc@icloud.com>
2022-03-01 12:28:24 +0000

    Solved problems using freecodecamp tutorial.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+	var splitString = str.split('');
+	var reverseArray = splitString.reverse();
+	var joinArray = reverseArray.join('');
+	return joinArray;
 };
+reverseString('hello there') // returns 'ereht olleh'
 
 // Do not edit below this line
 module.exports = reverseString;
