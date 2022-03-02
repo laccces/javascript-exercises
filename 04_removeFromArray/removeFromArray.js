b6f4c305fb467f857919933845bd30b79eedd0bf
Dan <en07djc@icloud.com>
2022-03-02 14:51:22 +0000

    Completed exercises 4 & 5.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+function removeFromArray() {
+	var arguments = Number(arguments);
+    var arr = arguments.length && arguments[0] || [];
+	var targets = {};
+	for (var i = 1; i < arguments.length; i++) {
+		targets[arguments[i]] = true;
+		}
+	return arr.filter(function (x) {
+		return targets[x] === undefined;
+		});
+		}
 
-};
+// Number(arguments)
+// if (Number(arguments) = NaN)
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+function sumAll(x, y) {
+if (x < 0 || y < 0) return "ERROR";
+if (typeof x != "number" || typeof y != "number") return "ERROR";
 
-};
+if (x > y) {
+    n1 = x
+    n2 = y
+} else {
+    n1 = y
+    n2 = x
+}
+
+return (n1 + n2)*(n1/2);
+
+}
+
+sumAll(133, 1);
 
 // Do not edit below this line
 module.exports = sumAll;
