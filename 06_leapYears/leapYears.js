86afdd95c8b7134b0d738fbd4c9c6e133a437cf2
Dan <en07djc@icloud.com>
2022-03-03 13:36:36 +0000

    Completed task 6 with minimal changes!

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
+function leapYears(year) {
 
-};
+    if (year % 100 == 0 && year % 400 !== 0) return false;
+    else if (year % 4 == 0 || year % 400 == 0) return true;
+    else {return false}
+    }
+
+leapYears(2000) // is a leap year: returns true
+leapYears(1985) // is not a leap year: returns false
 
 // Do not edit below this line
 module.exports = leapYears;
