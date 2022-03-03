9c89c1d4814c48acdecaf3579c8e0b6cbdbc5d78
Dan <en07djc@icloud.com>
2022-03-03 14:02:31 +0000

    Completed exercise 7.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
-
-};
-
-const ctof = function() {
-
-};
+function ftoc(fnum) {
+  var fsum = (fnum - 32)*(5/9);
+  var rounded = Math.round(fsum * 10)/10;
+  return rounded;
+  };
+  
+  
+  function ctof(cnum) {
+  var csum = (cnum)*(9/5)+32;
+  var rounded = Math.round(csum * 10)/10;
+  return rounded;
+  
+  };
 
 // Do not edit below this line
 module.exports = {
